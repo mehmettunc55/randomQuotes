999c13a08d6a01e8fca5cea511cda24f42dc29d0
mehmettunc55 <mtunc551980@gmail.com>
2021-04-23 21:49:19 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const quotes = [
+    {
+      quote:
+        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+      author: " Life"
+    },
+    {
+      quote:
+        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+      author: "John Kenneth Galbraith"
+    },
+    {
+      quote:
+        "God save me from my friends. I can protect myself from my enemies.",
+      author: "Claude Louis Hector de Villars "
+    },
+    {
+      quote: "The price of anything is the amount of life you exchange for it.",
+      author: "David Thoreau"
+    },
+    {
+      quote:
+        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+      author: "Charles Lindbergh"
+    },
+    {
+      quote:
+        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+      author: " Tyne Daly"
+    }
+  ];
+
+
+const button = document.querySelector('#generate-btn');
+const valueQuote = document.querySelector('#quote');
+const valueAuthor = document.querySelector('.quote-author');
+
+button.addEventListener('click', function(){
+  let randomText = Math.floor(Math.random()*quotes.length);
+
+  valueQuote.textContent = quotes[randomText].quote;
+  valueAuthor.textContent = quotes[randomText].author;
+}) ();
+
+
+
+
+
+
+/*   const button = document.getElementById('generate-btn');
+  button.addEventListener('click', changeText);
+
+  const valueQuote = document.querySelector('#quote');
+  const valueAuthor = document.querySelector('.author');
+
+  function changeText(){
+    let randomText = Math.floor(Math.random()* quotes.length);
+
+    valueQuote.textContent = quotes[randomText].quote;
+    valueAuthor.textContent = quotes[randomText].author;
+  } */
+
+
+
